49c7a5e5b755c9c2d603ad4200e680dd2f4c5a5f
Reynaldo Abreu <reynaldo.abreu@paysafe.com>
2017-12-12 11:12:32 +0000

    First Commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>About showing random images</h1>
+    <p>
+      We're the only site running on your computer dedicated to showing some random images that are about nothing in particular.
+    </p>
+  </div>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>Amazing Image of something random</h1>
+    <div>
+      <img
+        src="http://lorempixel.com/800/400/"
+        alt="A random image to be shown and discussed"
+      />
+    </div>
+  </div>
+);
